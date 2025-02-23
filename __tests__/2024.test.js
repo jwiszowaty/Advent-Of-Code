6ebfc0752984c56dffb4adbab1e4b77f1cb197a3
Jakub Wiszowaty <jakubwiszowaty@hotmail.com>
2025-02-23 19:58:25 +0000

    tests completed for one

---
+++ __tests__/2024.test.js
@@ -0,0 +1,28 @@
+const { sortNumbers, findDistance, sumDistances } = require("../2024/one.js")
+const fs = require("fs/promises")
+describe("1: Historian Hysteria", () => {
+    it("should sort numbers low to high", () => {
+        const unsortedNumbers = [4, 2, 3, 1, 0]
+        const sortedNumbers = [0, 1, 2, 3, 4]
+        const result = sortNumbers(unsortedNumbers)
+        expect(result).toEqual(sortedNumbers)
+    })
+    it("should sort numbers low to high for first nested array then the next", () => {
+        const unsortedNumbers = [[4, 2, 3, 1, 0],[9, 7, 8, 6, 5]]
+        const sortedNumbers = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+        const result = sortNumbers(unsortedNumbers)
+        expect(result).toEqual(sortedNumbers)
+    })
+    it("should return a new array with differences between correlated numbers", () => {
+        const unsortedNumbers = [[4, 2, 3, 1, 0],[9, 7, 8, 6, 5]]
+        const expected = [5,5,5,5,5]
+        const result = findDistance(sortNumbers(unsortedNumbers))
+        expect(result).toEqual(expected)
+    })
+    it("should return a sum of the distances", () => {
+        const unsortedNumbers = [[4, 2, 3, 1, 0],[9, 7, 8, 6, 5]]
+        const expected = 25
+        const result = sumDistances(findDistance(sortNumbers(unsortedNumbers)))
+        expect(result).toEqual(expected)
+    })
+})

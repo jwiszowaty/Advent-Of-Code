8ab30db0a635991c9d9e11dec4f5914ffcb84e2d
Jakub Wiszowaty <jakubwiszowaty@hotmail.com>
2025-04-23 22:01:53 +0100

    started on day 5 part 1 2024

---
+++ 2024/5.1.js
@@ -0,0 +1,8 @@
+//Identify the correct updates
+//// create an array for rules
+//// make each update an array
+//// check run rules against the update array using finding index of the numbers with rules array.indexOf(x)<array.indexOf(y)
+//// if a number doesn't appear in the update return true
+//// update is correctly ordered if all tests return true
+//// get its middle page number
+//Sum middle pages from correct updates
